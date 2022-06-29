dc56e61cf2602f748d4ce32d0c5e9a94cf6e3006
Abhishek Dileep <abhishekdileep99@gmail.com>
2022-06-29 07:45:25 +0530

    724 completed

--- 724.find-pivot-index.cpp
+++ 724.find-pivot-index.cpp
@@ -8,8 +8,20 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+       int sum = 0 , left  = 0 ; 
+       // sum of all elements in the array
+       for( int i = 0 ; i < nums.size() ;  i++ ) sum = sum + nums[i] ;
+        //Checking if the difference are equal to the sum of all elements in the array
+       for (int i = 0 ; i < nums.size() ; i++ ){
+        if(left == sum - left - nums[i]){
+            return i ; 
+        }
+        left = left + nums[i] ;
+       }
+       return -1;
     }
 };
+
+
 // @lc code=end
 
