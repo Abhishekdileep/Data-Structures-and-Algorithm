29b6fafad06609d7a065b8e338a93baa9a3c335c
Abhishek Dileep <abhishekdileep99@gmail.com>
2022-06-29 07:15:38 +0530

    724

---
+++ 724.find-pivot-index.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=724 lang=cpp
+ *
+ * [724] Find Pivot Index
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
